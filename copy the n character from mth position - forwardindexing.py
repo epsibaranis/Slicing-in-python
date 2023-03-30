6c7f5f0b2ce838395183aba2591dce21eee062b4
epsibaranis <104891770+epsibaranis@users.noreply.github.com>
2023-03-30 07:20:03 +0530

    fc

---
+++ copy the n character from mth position - forwardindexing.py
@@ -0,0 +1,8 @@
+# copy the n character from mth position - forwardindexing
+s1=input('s1=?')
+s2=''
+m=int(input('m=?'))
+n=int(input('n=?'))
+k=m+n
+s2=s1[m:k]
+print("copy the n character from mth position - forwardindexing",s2)
